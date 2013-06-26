054fa5837911f8b58d61cbb334e399eda6c4782c
Mike Howe <dockter@almuramc.com>
2013-06-25 20:07:40 -0500

    More Cleanup.
    Signed-off-by: Mike Howe <dockter@almuramc.com>

--- src/net/Dockter/HotBarReload/Util.java
+++ src/net/Dockter/HotBarReload/Util.java
@@ -7,38 +7,32 @@ import org.bukkit.entity.Player;
 public class Util
 {
   @SuppressWarnings("deprecation")
-public static void ReloadItemBar(Player player, Material itemType)
-  {
+public static void ReloadItemBar(Player player, Material itemType) {
     int destSlot = -1;
     int srcSlot = -1;
 
     destSlot = player.getInventory().getHeldItemSlot();
     srcSlot = getSlot(destSlot, player.getInventory().all(itemType));
 
-    if (srcSlot >= 0)
-    {
+    if (srcSlot >= 0) {
       player.getInventory().setItem(destSlot, player.getInventory().getItem(srcSlot));
       player.getInventory().clear(srcSlot);
       player.updateInventory();
     }
   }
 
-  private static int getSlot(int destSlot, HashMap<Integer, ?> items)
-  {
+  private static int getSlot(int destSlot, HashMap<Integer, ?> items) {
     int count = 0;
     int srcSlot = -1;
-    if (items != null)
-    {
-      if (items.size() > 0)
-      {
-        do
-        {
-          if (items.size() > count)
-          {
+    
+    if (items != null) {
+      if (items.size() > 0) {
+        do {
+          if (items.size() > count) {
             srcSlot = ((Integer)items.keySet().toArray()[count]).intValue();
           }
           count++;
-        }while ((srcSlot == destSlot) && (items.size() > count));
+        } while ((srcSlot == destSlot) && (items.size() > count));
       }
     }
     return srcSlot;
